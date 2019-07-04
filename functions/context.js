60c3b0b859c8cc1f6dd7902c7b23c6b263f00929
Tony Alves <tony@3alves.com>
2019-07-04 09:59:03 -0700

    in logs

--- functions/context.js
+++ functions/context.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
+  console.log(`{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`)
   callback(null, {
   statusCode: 200,
-  body: `{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`
+  body: `Your IP address ${event.headers.client-ip}`
   });
 }
