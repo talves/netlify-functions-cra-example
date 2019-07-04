6b61920e6f2582e629c3503b69aec1de9e43e5e6
Tony Alves <tony@3alves.com>
2019-07-04 09:10:20 -0700

    add event

--- functions/context.js
+++ functions/context.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
   statusCode: 200,
-  body: `${JSON.stringify(context,null,2)}`
+  body: `{\ncontext: ${JSON.stringify(context,null,2)},\nevent: ${JSON.stringify(event,null,2)}\n}`
   });
 }
